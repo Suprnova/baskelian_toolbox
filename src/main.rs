d8b05864943099f7d07f5c55171909fe72df39bc
Nova <nova@suprnova.dev>
2023-03-20 19:24:45 -0400

    Implement extracting inner DAT files

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,15 @@ fn main() {
     let input_trimmed = s.trim();
     let file = File::open(input_trimmed).unwrap();
     let dat = DAT::from_file(file).unwrap();
-    for (i, entry) in dat.table_entries.as_slice().iter().enumerate() {
-        let file = dat.read_entry(&entry).unwrap();
-        let file_path = Path::new(&input_trimmed).parent().unwrap().join(i.to_string());
-        std::fs::write(file_path, file).unwrap();
-        println!("Finished file {i}");
+    for (i, inner_dat) in dat.inner_dats.as_slice().iter().enumerate() {
+        for (j, entry) in inner_dat.file_table_entries.as_slice().iter().enumerate() {
+            let file = dat.read_file(inner_dat, entry).unwrap();
+            if file.len() == 0 {
+                continue;
+            }
+            let file_path = Path::new(&input_trimmed).parent().unwrap().join(format!("{}/{}", i, j));
+            std::fs::write(file_path, file).unwrap();
+            println!("Finished file {i}-{j}");
+        }
     }
 }

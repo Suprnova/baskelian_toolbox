56ab7ff9e078f0467dd0670e3d54c56864521c16
Nova <nova@suprnova.dev>
2023-03-20 17:23:14 -0400

    Modify reading logic

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@ pub mod dat_file {
     pub struct DAT {
         // The file that the DAT originates from
         file: Option<File>,
-        /// The first byte of the file, unknown meaning, likely a magic byte
-        unknown1: [u8; 1],
+        /// The number of entries in this file
+        entry_count: u32,
         /// The table of each file's info and offset within the file
         pub table_entries: Vec<TableEntry>,
     }
@@ -19,8 +19,11 @@ pub mod dat_file {
             let mut dat_struct: Self = Default::default();
             dat_struct.file = Some(file);
             let mut file = dat_struct.file.as_ref().unwrap();
-            file.read_exact(&mut dat_struct.unknown1)?;
+            let mut count_buffer: [u8; 4] = [0; 4];
+            file.read_exact(&mut count_buffer)?;
+            dat_struct.entry_count = u32::from_le_bytes(count_buffer);
             let mut buffer: [u8; 12] = [255; 12];
+            // could simply use entry_count instead of waiting for an empty buffer
             while buffer != [0; 12] {
                 // this throws an error if even one table is messed up, we could be more lenient with something like that
                 file.read_exact(&mut buffer)?;
@@ -41,29 +44,20 @@ pub mod dat_file {
 
     #[allow(dead_code)]
     pub struct TableEntry {
-        /// Unknown
-        unknown1: u8,
-        /// Unknown
-        unknown2: u8,
-        /// Unknown
-        unknown3: u8,
         /// The memory address within the DAT in which the file appears
         pub address: u32,
         /// The size of the file
         pub size: u32,
-        /// The first byte of the file, likely the magic byte, used to identify the file type?
-        pub first_byte: u8,
+        /// The number of entries in the file
+        pub entry_count: u32
     }
 
     impl TableEntry {
         fn from_entry(entry: [u8; 12]) -> Self {
             Self {
-                unknown1: entry[0],
-                unknown2: entry[1],
-                unknown3: entry[2],
-                address: u32::from_le_bytes(entry[3..7].try_into().expect("invalid table entry address")),
-                size: u32::from_le_bytes(entry[7..11].try_into().expect("invalid table entry size")),
-                first_byte: entry[11],
+                address: u32::from_le_bytes(entry[0..4].try_into().expect("invalid table entry address")),
+                size: u32::from_le_bytes(entry[4..8].try_into().expect("invalid table entry size")),
+                entry_count: u32::from_le_bytes(entry[8..12].try_into().expect("invalid entry count")),
             }
         }
     }

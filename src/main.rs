597bb65b42bed4ebfe562daa688478d362617176
colin <colinerickson@live.com>
2024-01-27 02:05:05 -0500

    Implements functional work-in-progress StatsEntry objects for most stat entries

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::stdin, path::Path};
 
-use baskelian_toolbox::dat::DAT;
+use baskelian_toolbox::dat::{DAT, FileType};
 fn main() {
     let mut s = String::new();
     println!("Please enter the path to the DATA.DAT file.");
@@ -13,6 +13,9 @@ fn main() {
             let file = dat.read_file(inner_dat, entry).unwrap();
             let file_path = Path::new(&input_trimmed).parent().unwrap().join(format!("{i}-{j}{}", file.file_type));
             std::fs::write(file_path, file.data).unwrap();
+            //if let FileType::STATS(stats_file) = file.file_type {
+            //    dbg!(stats_file);
+            //}
             println!("Finished file {i}-{j}{}", file.file_type);
         }
     }

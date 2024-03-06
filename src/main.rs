c016abe46bd32589d4763010353e1be649e0caae
Suprnova <nova@suprnova.dev>
2024-03-06 12:33:30 -0500

    Use _artifacts for DATA.DAT

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::stdin, path::Path};
+use std::{fs::{create_dir, File}, path::Path};
 
 extern crate num_derive;
 extern crate encoding_rs;
@@ -6,17 +6,20 @@ extern crate encoding_rs;
 
 use baskelian_toolbox::dat::{DAT, FileType};
 fn main() {
-    let mut s = String::new();
-    println!("Please enter the path to the DATA.DAT file.");
-    stdin().read_line(&mut s).expect("Error parsing user input");
-    let input_trimmed = s.trim();
-    let dat_file = File::open(input_trimmed).unwrap();
+    let dat_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("_artifacts/DATA.DAT");
+    let extract_err = create_dir(dat_path.parent().unwrap().join("extracted")).err();
+    if extract_err.is_some() {
+        if !Path::new(&dat_path.parent().unwrap().join("extracted")).exists() {
+            panic!("unknown directory error")
+        }
+    }
+    let dat_file = File::open(&dat_path).expect("DATA.DAT file not found in _artifacts folder!");
     let dat = DAT::from_file(dat_file).unwrap();
     for (i, inner_dat) in dat.inner_dats.as_slice().iter().enumerate() {
         let mut archive_name: Option<String> = None;
         for (j, entry) in inner_dat.file_table_entries.as_slice().iter().enumerate() {
             let mut file = dat.read_file(inner_dat, entry).unwrap();
-            let mut file_path = Path::new(&input_trimmed).parent().unwrap().join(format!("{i}-{j}{}", file.file_type));
+            let mut file_path = Path::new(&dat_path).parent().unwrap().join(format!("extracted/{i}-{j}{}", file.file_type));
             // file has file name, so all files in this archive have same file name
             // (temporary for put2d scripts, should be implemented library-side)
             if file.file_name.is_some() {
@@ -28,7 +31,7 @@ fn main() {
                 if matches!(file.file_type, FileType::UNKNOWN) {
                     file.file_type = FileType::TXT
                 }
-                file_path = Path::new(&input_trimmed).parent().unwrap().join(format!("{}{}", archive_name.clone().unwrap(), file.file_type));
+                file_path = Path::new(&file_path).parent().unwrap().join(format!("{}{}", archive_name.clone().unwrap(), file.file_type));
             }
             std::fs::write(file_path.clone(), file.data).unwrap();
             println!("Finished file {}", file_path.to_str().unwrap().split("\\").last().unwrap());

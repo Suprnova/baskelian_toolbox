f9e3c52513834d904151da02d1f75b2df8509cd9
Suprnova <nova@suprnova.dev>
2024-03-06 10:19:13 -0500

    More file type detection

--- src/lib.rs
+++ src/lib.rs
@@ -161,22 +161,33 @@ pub mod dat {
     pub enum FileType {
         /// RenderWare Anim Animation File (0x1B)
         ANM,
+        /// Baskelian MIF Attached Data Script
+        ATTACHED,
+        /// Baskelian MIF Comid Data Script
+        COMID,
         /// RenderWare Delta Morph Animation File (0x1E)
         DMA,
         /// RenderWare Model File (Clump) (0x10)
         DFF,
+        /// Baskelian MIF Fixed Data Script
+        FIXED,
         /// Baskelian Font Type Information
         FTI,
         /// Baskelian Map Info File
         MAPINFO,
+        /// Baskelian Name Data Script
+        NAME,
         /// Portable Network Graphics File
         PNG,
+        /// Baskelian MIF PostBL Data Script
+        POSTBL,
         /// Baskelian Put2D Script
         PUT2D{put2d_script: put2d::Put2D},
         /// Baskelian Stats File
         STATS{stats_file: stats::Stats},
         /// RenderWare Texture Dictionary (0x16)
         TXD,
+        TXT,
         UNKNOWN,
     }
 
@@ -185,6 +196,7 @@ pub mod dat {
             if data.is_empty() {
                 Self::UNKNOWN
             } else {
+                // TODO: Make this code cleaner
                 match data[0] {
                     0x10 => Self::DFF,
                     0x16 => Self::TXD,
@@ -195,8 +207,39 @@ pub mod dat {
                         if data.len() >= 4 && data[1..4] == [0x50, 0x4E, 0x47] {
                             return Self::PNG;
                         }
-                        if data.len() >= 12 && data[0..12] == [0x70, 0x75, 0x74, 0x32, 0x64, 0x2D, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74] {
+                        // put2d signature
+                        if data.len() > 12 && data[0..12] == [0x70, 0x75, 0x74, 0x32, 0x64, 0x2D, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74] {
                             return Self::PUT2D { put2d_script: put2d::Put2D::from_data(data).unwrap()}
+                        }
+                        // mif fixed signature
+                        if data.len() > 21 && data[0..21] == [
+                            0x6D, 0x69, 0x66, 0x2D, 0x66, 0x69, 0x78, 0x65, 0x64, 0x2D, 0x64, 0x61, 0x74, 0x61, 0x2D, 0x73,
+                            0x63, 0x72, 0x69, 0x70, 0x74] {
+                            return Self::FIXED;
+                        }
+                        // mif attached signature
+                        if data.len() > 24 && data[0..24] == [
+                            0x6D, 0x69, 0x66, 0x2D, 0x61, 0x74, 0x74, 0x61, 0x63, 0x68, 0x65, 0x64, 0x2D, 0x64, 0x61, 0x74, 
+                            0x61, 0x2D, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74] {
+                            return Self::ATTACHED;
+                        }
+                        // mif postbl signature
+                        if data.len() > 22 && data[0..22] == [
+                            0x6D, 0x69, 0x66, 0x2D, 0x70, 0x6F, 0x73, 0x74, 0x62, 0x6C, 0x2D, 0x64, 0x61, 0x74, 0x61, 0x2D, 
+                            0x73, 0x63, 0x72, 0x69, 0x70, 0x74] {
+                            return Self::POSTBL;
+                        }
+                        // mif comid signature
+                        if data.len() > 21 && data[0..21] == [
+                            0x6D, 0x69, 0x66, 0x2D, 0x63, 0x6F, 0x6D, 0x69, 0x64, 0x2D, 0x64, 0x61, 0x74, 0x61, 0x2D, 0x73, 
+                            0x63, 0x72, 0x69, 0x70, 0x74] {
+                            return Self::COMID;
+                        }
+                        // mif name signature
+                        if data.len() > 20 && data[0..20] == [
+                            0x6D, 0x69, 0x66, 0x2D, 0x6E, 0x61, 0x6D, 0x65, 0x2D, 0x64, 0x61, 0x74, 0x61, 0x2D, 0x73, 0x63, 
+                            0x72, 0x69, 0x70, 0x74] {
+                            return Self::NAME;
                         }
                         // check for "font-type" signature
                         if data.len() >= 20 && data[0..9] == [0x66, 0x6F, 0x6E, 0x74, 0x2D, 0x74, 0x79, 0x70, 0x65] {
@@ -242,14 +285,20 @@ pub mod dat {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             match self {
                 Self::ANM => write!(f, ".anm"),
+                Self::ATTACHED => write!(f, ".cv3"),
+                Self::COMID => write!(f, ".comid"),
                 Self::DMA => write!(f, ".dma"),
                 Self::DFF => write!(f, ".dff"),
+                Self::FIXED => write!(f, ".fix"),
                 Self::FTI => write!(f, ".fti"),
                 Self::MAPINFO => write!(f, ".mapinfo"),
+                Self::NAME => write!(f, ".name"),
                 Self::PNG => write!(f, ".png"),
+                Self::POSTBL => write!(f, ".postbl"),
                 Self::PUT2D{..} => write!(f, ".put2d"),
                 Self::STATS{..} => write!(f, ".stats"),
                 Self::TXD => write!(f, ".txd"),
+                Self::TXT => write!(f, ".txt"),
                 Self::UNKNOWN => write!(f, ""),
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,19 @@ fn main() {
     for (i, inner_dat) in dat.inner_dats.as_slice().iter().enumerate() {
         let mut archive_name: Option<String> = None;
         for (j, entry) in inner_dat.file_table_entries.as_slice().iter().enumerate() {
-            let file = dat.read_file(inner_dat, entry).unwrap();
-            /*if let FileType::PUT2D{put2d_script} = &file.file_type {
-                //dbg!(put2d_script);
-            } else if archive_name.is_none() {
-                continue;
-            }*/
+            let mut file = dat.read_file(inner_dat, entry).unwrap();
             let mut file_path = Path::new(&input_trimmed).parent().unwrap().join(format!("{i}-{j}{}", file.file_type));
+            // file has file name, so all files in this archive have same file name
+            // (temporary for put2d scripts, should be implemented library-side)
             if file.file_name.is_some() {
                 archive_name = file.file_name;
             }
             if archive_name.is_some() {
+                // an unknown file in a known archive_name must be a .txt
+                // (temporary for put2d scripts)
+                if matches!(file.file_type, FileType::UNKNOWN) {
+                    file.file_type = FileType::TXT
+                }
                 file_path = Path::new(&input_trimmed).parent().unwrap().join(format!("{}{}", archive_name.clone().unwrap(), file.file_type));
             }
             std::fs::write(file_path.clone(), file.data).unwrap();
